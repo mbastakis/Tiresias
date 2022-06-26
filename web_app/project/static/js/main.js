f3ad1da633021b8eac7a07da2678af6f93482519
MICHAIL BASTAKIS <mbastakis@gmail.com>
2022-06-27 00:28:59 +0300

    Added frontend to the app

--- web_app/project/static/js/main.js
+++ web_app/project/static/js/main.js
@@ -1,40 +1,116 @@
-var questionCounter = 0;
+var sideBar = document.getElementById("mobile-nav");
+var openSidebar = document.getElementById("openSideBar");
+var closeSidebar = document.getElementById("closeSideBar");
+sideBar.style.transform = "translateX(-260px)";
+
+var questionCounter = 1
+var haveContext = true
+
+function sidebarHandler(flag) {
+    if (flag) {
+        sideBar.style.transform = "translateX(0px)";
+        openSidebar.classList.add("hidden");
+        closeSidebar.classList.remove("hidden");
+    } else {
+        sideBar.style.transform = "translateX(-260px)";
+        closeSidebar.classList.add("hidden");
+        openSidebar.classList.remove("hidden");
+    }
+}
+
+function applyChanges() {
+    className = $(this).attr('id');
+    if (className.includes('small')) {
+        $('#' + className.substring(0, className.length - 6)).val($('#' + className).val());
+    } else {
+        $('#' + className + '-small').val($('#' + className).val());
+    }
+}
+
+$('#model').on('change', applyChanges);
+$('#model-small').on('change', applyChanges);
+$('#lang').on('change', applyChanges);
+$('#lang-small').on('change', applyChanges);
+$('#trans').on('change', applyChanges);
+$('#trans-small').on('change', applyChanges);
+$('#erm').on('change', applyChanges);
+$('#erm-small').on('change', applyChanges);
+
+$('.context-toggle').on('click', function () {
+    let isOn = !$(this).is(":checked");
+
+    if (isOn) {
+        $('#erm').hide();
+        $('#erm-small').hide();
+        $('#context').hide();
+        $(this).prop('checked', false);
+        haveContext = false;
+    } else {
+        $('#erm').show();
+        $('#erm-small').show();
+        $('#context').show();
+        $(this).prop('checked', true);
+        haveContext = true;
+    }
+
+    className = $(this).attr('id');
+    if (className.includes('small')) {
+        $('#' + className.substring(0, className.length - 6)).prop('checked', !isOn);
+    } else {
+        $('#' + className + '-small').prop('checked', !isOn);
+    }
+});
 
 $('#add-button').on('click', () => {
     $questions = $('.questions-section');
     questionCounter++;
 
-    $questions.append('<div class="question" id="' + questionCounter + '">' +
-        '<h3 class="question-counter">Question #' + (questionCounter + 1) + '</h3>' +
-        '<div class="question-field">' +
-        '<input class="input" type="text" value="">' +
-        '<button class="remove-question" id="remove-' + questionCounter + '" name="' + questionCounter + '"> - </button>' +
+    $questions.append(
+        '<div id = "' + questionCounter + '" class="flex flex-row items-center ">' +
+        '<div class="form-control w-full max-w-xl mb-5" >' +
+        '<label class="label question-label">' +
+        '<span class="label-text text-lg">Question ' + (questionCounter) + '</span>' +
+        '</label>' +
+        '<div class="w-full flex flex-col">' +
+        '<input type="text" placeholder="Write a question..." class="input input-bordered w-full input-question" />' +
+        '<result class="bg-primary p-3 -mt-1 -z-10 rounded-t-none rounded-b-lg text-gray-200"></result>' +
         '</div>' +
-        '<output></output>');
+        '</div>' +
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" id="remove-' + questionCounter + '" class="mt-5 h-5 ml-4 hover:cursor-pointer"><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z" /></svg>' +
+        '</div >'
+    );
+
+    question_list = $('.question-label');
+    for (let i = 0; i < question_list.length; i++) {
+        $(question_list.get(i))[0].innerHTML = '<span class="label-text text-lg">Question ' + (i + 1) + '</span>';
+    }
 
     $('#remove-' + questionCounter).on('click', (question) => {
-        var removeId = question.currentTarget["name"];
+        let removeId = $($(question.currentTarget)[0]).attr('id').replace('remove-', '')
         $('#' + removeId).remove();
-        question_list = $('.question-counter');
+        question_list = $('.question-label');
         for (let i = 0; i < question_list.length; i++) {
-            question_list.get(i).innerText = 'Question #' + (i + 2);
+            $(question_list.get(i))[0].innerHTML = '<span class="label-text text-lg">Question ' + (i + 1) + '</span>';
         }
     });
-
-    question_list = $('.question-counter');
-    for (let i = 0; i < question_list.length; i++) {
-        question_list.get(i).innerText = 'Question #' + (i + 2);
-    }
 });
 
-$('#answer-button').on('click', () => {
-    outputList = $('output');
-    inputList = $('.input');
-    context = $('#context').val();
-    $('load-bar').addClass("loader");
 
-    model = $('#models').val();
-    lang = $('#lang').val();
+
+
+
+$('#answer-button').on('click', () => {
+    let outputList = $('result');
+    let questionList = $('.input-question');
+    let context = $('#context').val();
+    let model = $('#model').val();
+    let lang = $('#lang').val();
+    let trans = $('#trans').val();
+    let erm = $('#erm').val();
+
+
+    // $('load-bar').addClass("loader");
+
 
     let request = {};
     request.context = context;
@@ -45,23 +121,23 @@ $('#answer-button').on('click', () => {
         request.questions.push(inputList.get(i).value);
     }
 
-    $.ajax({
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(request),
-        dataType: 'json',
-        url: 'http://127.0.0.1:8080/questions',
-        success: (answers) => {
-            $('load-bar').removeClass("loader");
-            i = 0;
-            for (let index in answers) {
-                outputList.get(i).innerText = answers[index];
-                i++;
-            }
-        },
-        error: (e) => {
-            $('load-bar').removeClass("loader");
-            console.log(e);
-        }
-    });
+    // $.ajax({
+    //     type: 'POST',
+    //     contentType: 'application/json',
+    //     data: JSON.stringify(request),
+    //     dataType: 'json',
+    //     url: 'http://127.0.0.1:8080/questions',
+    //     success: (answers) => {
+    //         $('load-bar').removeClass("loader");
+    //         i = 0;
+    //         for (let index in answers) {
+    //             outputList.get(i).innerText = answers[index];
+    //             i++;
+    //         }
+    //     },
+    //     error: (e) => {
+    //         $('load-bar').removeClass("loader");
+    //         console.log(e);
+    //     }
+    // });
 });
